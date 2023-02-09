228b70ddc0bc662124c3a67a0b4cf7c4bdcf974a
Christian Maikowski <c.maikowski@web.de>
2023-02-09 19:55:45 +0100

    Added Model and Mock Repository

--- src/CommandAPI/Program.cs
+++ src/CommandAPI/Program.cs
@@ -1,6 +1,11 @@
+using CommandAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
+
 var app = builder.Build();
 
 // app.MapGet("/", () => "Hello World!");
