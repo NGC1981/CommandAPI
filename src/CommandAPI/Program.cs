3f28dc6c1bcf4b4a7552ec3c353c6d9aae82c120
Christian Maikowski <c.maikowski@web.de>
2023-02-12 00:24:05 +0100

    Connection string secured

--- src/CommandAPI/Program.cs
+++ src/CommandAPI/Program.cs
@@ -1,12 +1,18 @@
 using CommandAPI.Data;
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 
 ConfigurationManager configuration = builder.Configuration; // allows both to access and to set up the config
 IWebHostEnvironment environment = builder.Environment;
 
-builder.Services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(configuration.GetConnectionString("PostgreSqlConnection")));
+var b = new NpgsqlConnectionStringBuilder();
+b.ConnectionString = configuration.GetConnectionString("PostgreSqlConnection");
+b.Username = configuration["UserID"];
+b.Password = configuration["Password"];
+
+builder.Services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(b.ConnectionString));
 
 builder.Services.AddControllers();
 
