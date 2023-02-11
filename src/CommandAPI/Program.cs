e9a80b31de45e84da4d1706625ee4e1b915e92ac
Christian Maikowski <c.maikowski@web.de>
2023-02-11 20:44:50 +0100

    Added CommandContext to Solution

--- src/CommandAPI/Program.cs
+++ src/CommandAPI/Program.cs
@@ -1,10 +1,17 @@
 using CommandAPI.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+ConfigurationManager configuration = builder.Configuration; // allows both to access and to set up the config
+IWebHostEnvironment environment = builder.Environment;
+
+builder.Services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(configuration.GetConnectionString("PostgreSqlConnection")));
+
 builder.Services.AddControllers();
 
-builder.Services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
+//builder.Services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
+builder.Services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
 
 var app = builder.Build();
 
